dd4720e3fecdb90d5af5b003470cdd4735b47c33
DavidWu421 <dwuuu.421@gmail.com>
2022-08-02 07:42:22 -0700

    Add for loop to generate_block_logical_coords

--- src/Executables/ExtendConnectivity/ExtendConnectivity.cpp
+++ src/Executables/ExtendConnectivity/ExtendConnectivity.cpp
@@ -12,6 +12,7 @@
 #include "Domain/LogicalCoordinates.hpp"
 #include "IO/H5/AccessType.hpp"
 #include "IO/H5/File.hpp"
+#include "IO/H5/Helpers.hpp"
 #include "IO/H5/VolumeData.hpp"
 #include "NumericalAlgorithms/Spectral/Mesh.hpp"
 #include "NumericalAlgorithms/Spectral/Spectral.hpp"
@@ -24,93 +25,146 @@
 // main module we just have it be empty
 extern "C" void CkRegisterMainModule(void) {}
 
-std::pair<Mesh<3>, std::string> generate_mesh(const h5::VolumeData& volume_file,
-                                              const size_t& single_id,
-                                              const size_t element_number) {
+std::tuple<size_t, size_t, std::tuple<int, int, int>>
+compute_expected_connectivity_length(const h5::VolumeData& volume_file,
+                                     const size_t& single_obs_id,
+                                     const size_t total_number_of_elements) {
+  size_t expected_connectivity_length = 0;
+  size_t expected_number_of_grid_points = 0;
+  for (size_t i = 0; i < total_number_of_elements; ++i) {
+    auto extents = volume_file.get_extents(single_obs_id);
+    expected_connectivity_length +=
+        (extents[i][0] - 1) * (extents[i][1] - 1) * (extents[i][2] - 1) * 8;
+    expected_number_of_grid_points +=
+        extents[i][0] * extents[i][1] * extents[i][2];
+  }
+
+  std::string grid_name_string = volume_file.get_grid_names(single_obs_id)[0];
+
+  size_t h_ref_x_start_position = grid_name_string.find("L", 0);
+  size_t h_ref_y_start_position =
+      grid_name_string.find("L", h_ref_x_start_position + 1);
+  size_t h_ref_z_start_position =
+      grid_name_string.find("L", h_ref_y_start_position + 1);
+
+  size_t h_ref_x_end_position = grid_name_string.find("I", 0);
+  size_t h_ref_y_end_position =
+      grid_name_string.find("I", h_ref_x_end_position + 1);
+  size_t h_ref_z_end_position =
+      grid_name_string.find("I", h_ref_y_end_position + 1);
+
+  int h_ref_x = std::stoi(grid_name_string.substr(
+      h_ref_x_start_position + 1,
+      h_ref_x_end_position - h_ref_x_start_position - 1));
+  int h_ref_y = std::stoi(grid_name_string.substr(
+      h_ref_y_start_position + 1,
+      h_ref_y_end_position - h_ref_y_start_position - 1));
+  int h_ref_z = std::stoi(grid_name_string.substr(
+      h_ref_z_start_position + 1,
+      h_ref_z_end_position - h_ref_z_start_position - 1));
+
+  expected_connectivity_length +=
+      ((pow(2, h_ref_x + 1) - 1) * (pow(2, h_ref_y + 1) - 1) *
+           (pow(2, h_ref_z + 1) - 1) -
+       total_number_of_elements) *
+      8;
+
+  std::tuple<int, int, int> h_refinement_tuple{h_ref_x, h_ref_y, h_ref_z};
+
+  return std::tuple{expected_connectivity_length,
+                    expected_number_of_grid_points, h_refinement_tuple};
+}
+
+std::pair<Mesh<3>, std::string> generate_element_properties(
+    const h5::VolumeData& volume_file, const size_t& single_obs_id,
+    const size_t element_number) {
   // MAKE SURE TO MAKE THE DIM GENERAL
-  auto bases = volume_file.get_bases(single_id);
+  auto bases = volume_file.get_bases(single_obs_id);
   std::array<Spectral::Basis, 3> basis_array = {
       Spectral::to_basis(bases[element_number][0]),
       Spectral::to_basis(bases[0][1]),
       Spectral::to_basis(bases[element_number][2])};
-  // std::cout << bases << '\n' << bases.size() << '\n';
 
-  auto quadratures = volume_file.get_quadratures(single_id);
+  auto quadratures = volume_file.get_quadratures(single_obs_id);
   std::array<Spectral::Quadrature, 3> quadrature_array = {
       Spectral::to_quadrature(quadratures[element_number][0]),
       Spectral::to_quadrature(quadratures[element_number][1]),
       Spectral::to_quadrature(quadratures[element_number][2])};
-  // std::cout << quadratures << '\n' << quadratures.size() << '\n';
 
-  auto extents = volume_file.get_extents(single_id);
+  auto extents = volume_file.get_extents(single_obs_id);
   std::array<size_t, 3> extents_array = {extents[element_number][0],
                                          extents[element_number][1],
                                          extents[element_number][2]};
-  // std::cout << extents << '\n' << extents.size() << '\n';
 
-  auto grid_names = volume_file.get_grid_names(single_id);
+  auto grid_names = volume_file.get_grid_names(single_obs_id);
   std::string grid_names_array = grid_names[element_number];
-  // std::cout << grid_names_array << '\n';
-  // std::cout << grid_names << '\n';
 
-  std::cout << "Finished generate_mesh" << '\n';
+  // Generates the mesh
   Mesh<3> element_mesh = Mesh<3>{extents_array, basis_array, quadrature_array};
-  std::pair<Mesh<3>, std::string> output(element_mesh, grid_names_array);
-  return output;
+
+  std::pair<Mesh<3>, std::string> element_properties(element_mesh,
+                                                     grid_names_array);
+
+  return element_properties;
 }
 
 // Compute Block-logical coordinates
-tnsr::I<DataVector, 3, Frame::BlockLogical> block_logical_coords(
+const tnsr::I<DataVector, 3, Frame::BlockLogical> generate_block_logical_coords(
     const tnsr::I<DataVector, 3, Frame::ElementLogical>& new_coords,
-    const std::string& element_id) {
-  std::string R_x_string(1, element_id[6]);  // HARDCODE
-  double R_x = std::stod(R_x_string);
-  std::string R_y_string(1, element_id[11]);  // HARDCODE
-  double R_y = std::stod(R_y_string);
-  std::string R_z_string(1, element_id[16]);  // HARDCODE
-  double R_z = std::stod(R_z_string);
-  double N_x = pow(2, R_x);
-  double N_y = pow(2, R_y);
-  double N_z = pow(2, R_z);
-  std::string index_x_string(1, element_id[8]);   // HARDCODE
-  std::string index_y_string(1, element_id[13]);  // HARDCODE
-  std::string index_z_string(1, element_id[18]);  // HARDCODE
-  int index_x = std::stoi(index_x_string);
-  int index_y = std::stoi(index_y_string);
-  int index_z = std::stoi(index_z_string);
-  double shift_x = (-1 + (2 * index_x + 1) / N_x);
-  double shift_y = (-1 + (2 * index_y + 1) / N_y);
-  double shift_z = (-1 + (2 * index_z + 1) / N_z);
-  DataVector block_logical_x = 1 / N_x * new_coords.get(0) + shift_x;
-  DataVector block_logical_y = 1 / N_y * new_coords.get(1) + shift_y;
-  DataVector block_logical_z = 1 / N_z * new_coords.get(2) + shift_z;
-  std::cout << element_id << '\n';
+    const std::string& element_id,
+    const std::tuple<int, int, int>& h_refinement_tuple) {
+  size_t grid_points_x_start_position = 0;
   tnsr::I<DataVector, 3, Frame::BlockLogical> block_logical_coords{
-      block_logical_x.size(), 0.};
-  block_logical_coords.get(0) = block_logical_x;
-  block_logical_coords.get(1) = block_logical_y;
-  block_logical_coords.get(2) = block_logical_z;
-  std::cout << block_logical_coords << '\n';
+      new_coords.get(0).size(), 0.};
+  for (size_t i = 0; i < 3; ++i) {
+    double number_of_elements = 0;
+    if (i == 0) {  // definitely a better way to do this. Can't use std::get<i>
+                   // though
+      number_of_elements = pow(2, std::get<0>(h_refinement_tuple));
+    }
+    if (i == 1) {
+      number_of_elements = pow(2, std::get<1>(h_refinement_tuple));
+    }
+    if (i == 2) {
+      number_of_elements = pow(2, std::get<2>(h_refinement_tuple));
+    }
+
+    size_t grid_points_start_position =
+        element_id.find("I", grid_points_x_start_position + 1);
+    size_t grid_points_end_position = 0;
+    if (i != 2) {
+      grid_points_end_position =
+          element_id.find(",", grid_points_start_position);
+    } else {
+      grid_points_end_position =
+          element_id.find(")", grid_points_start_position);
+    }
+    int element_index = std::stoi(element_id.substr(
+        grid_points_start_position + 1,
+        grid_points_end_position - grid_points_start_position - 1));
+    double shift = (-1 + (2 * element_index + 1) / number_of_elements);
+    DataVector block_logical =
+        1 / number_of_elements * new_coords.get(i) + shift;
+    block_logical_coords.get(i) = block_logical;
+    grid_points_x_start_position = grid_points_start_position;
+  }
 
   return block_logical_coords;
 }
 
 // Functions that compute the connectivity within the block
 std::map<std::tuple<double, double, double>, size_t> generate_grid_point_map(
-    const tnsr::I<DataVector, 3, Frame::ElementLogical>& coord_data) {
+    const tnsr::I<DataVector, 3, Frame::BlockLogical>& coord_data) {
   std::map<std::tuple<double, double, double>, size_t> grid_point_map;
   DataVector coord_data_x = coord_data.get(0);
   DataVector coord_data_y = coord_data.get(1);
   DataVector coord_data_z = coord_data.get(2);
   for (size_t i = 0; i < coord_data_x.size(); ++i) {
-    std::tuple<double, double, double> coord_data_point;
-    std::get<0>(coord_data_point) = coord_data_x[i];
-    std::get<1>(coord_data_point) = coord_data_y[i];
-    std::get<2>(coord_data_point) = coord_data_z[i];
+    std::tuple<double, double, double> coord_data_point(
+        coord_data_x[i], coord_data_y[i], coord_data_z[i]);
     grid_point_map.insert(std::pair<std::tuple<double, double, double>, size_t>(
         coord_data_point, i));
   }
-  std::cout << "Finished generate_grid_point_map" << '\n';
   return grid_point_map;
 }
 
@@ -129,7 +183,6 @@ std::vector<double> sort_and_order(DataVector& data_vector) {
       ordered_coords.push_back(data_list[i]);
     }
   }
-  std::cout << "Finished sort_and_order" << '\n';
   return ordered_coords;
 }
 
@@ -153,29 +206,26 @@ std::vector<std::tuple<double, double, double>> build_connectivity_by_element(
       }
     }
   }
-  std::cout << "Finished build_connectivity_by_element" << '\n';
   return connectivity_as_tuples;
 }
 
-std::vector<double> generate_new_connectivity(
-    tnsr::I<DataVector, 3, Frame::ElementLogical>& logical_coords) {
+void generate_new_connectivity(
+    tnsr::I<DataVector, 3, Frame::BlockLogical>& block_logical_coords,
+    std::vector<int>& new_connectivity) {
   std::map<std::tuple<double, double, double>, size_t> grid_point_map =
-      generate_grid_point_map(logical_coords);
-  DataVector logical_coords_x = logical_coords.get(0);
-  DataVector logical_coords_y = logical_coords.get(1);
-  DataVector logical_coords_z = logical_coords.get(2);
+      generate_grid_point_map(block_logical_coords);
+  DataVector logical_coords_x = block_logical_coords.get(0);
+  DataVector logical_coords_y = block_logical_coords.get(1);
+  DataVector logical_coords_z = block_logical_coords.get(2);
   std::vector<double> ordered_x = sort_and_order(logical_coords_x);
   std::vector<double> ordered_y = sort_and_order(logical_coords_y);
   std::vector<double> ordered_z = sort_and_order(logical_coords_z);
   std::vector<std::tuple<double, double, double>> connectivity_of_tuples =
       build_connectivity_by_element(ordered_x, ordered_y, ordered_z);
-  std::vector<double> connectivity;
+
   for (const std::tuple<double, double, double>& it : connectivity_of_tuples) {
-    connectivity.push_back(grid_point_map[it]);
+    new_connectivity.push_back(grid_point_map[it]);
   }
-  // std::cout << connectivity << '\n';
-  std::cout << "Finished generate_new_connectivity" << '\n';
-  return connectivity;
 }
 
 void block_connectivity(const std::string& file_name,
@@ -183,21 +233,42 @@ void block_connectivity(const std::string& file_name,
   h5::H5File<h5::AccessType::ReadWrite> data_file(file_name, true);
   const auto& volume_file = data_file.get<h5::VolumeData>("/" + subfile_name);
   auto observation_ids = volume_file.list_observation_ids();
-  const auto& single_id =
+  const auto& single_obs_id =
       observation_ids[0];  // Hard coded for only 1 observation ID
-  std::vector<double> new_connectivity;
-  for (size_t i = 0; i < volume_file.get_bases(single_id).size(); ++i) {
-    auto new_mesh = generate_mesh(volume_file, single_id, i);
-    auto new_coords = logical_coordinates(std::get<0>(new_mesh));
-    std::cout << new_coords << '\n';
-    std::cout << block_logical_coords(new_coords, std::get<1>(new_mesh))
-              << '\n';
-    auto generated_connectivity = generate_new_connectivity(new_coords);
-    new_connectivity.insert(new_connectivity.end(),
-                            generated_connectivity.begin(),
-                            generated_connectivity.end());
+  size_t total_number_of_elements =
+      volume_file.get_bases(single_obs_id)
+          .size();  // Check this using grid_names for faster parsing?
+
+  auto [expected_connectivity_length, expected_number_of_grid_points,
+        h_refinement_tuple] =
+      compute_expected_connectivity_length(volume_file, single_obs_id,
+                                           total_number_of_elements);
+
+  tnsr::I<DataVector, 3, Frame::BlockLogical> total_block_logical_coords{
+      expected_number_of_grid_points, 0.};
+
+  size_t offset = 0;
+  for (size_t i = 0; i < volume_file.get_bases(single_obs_id).size(); ++i) {
+    auto [element_mesh, element_id] =
+        generate_element_properties(volume_file, single_obs_id, i);
+    auto element_logical_coords = logical_coordinates(element_mesh);
+    auto block_logical_coords = generate_block_logical_coords(
+        element_logical_coords, element_id, h_refinement_tuple);
+    for (size_t j = 0 + offset;
+         j < (block_logical_coords.get(0).size() + offset); ++j) {
+      total_block_logical_coords.get(0)[j] =
+          block_logical_coords.get(0)[j - offset];
+      total_block_logical_coords.get(1)[j] =
+          block_logical_coords.get(1)[j - offset];
+      total_block_logical_coords.get(2)[j] =
+          block_logical_coords.get(2)[j - offset];
+    }
+    offset += block_logical_coords.get(0).size();
   }
-  // std::cout << new_connectivity << '\n';
+  std::vector<int> new_connectivity;
+  new_connectivity.reserve(expected_number_of_grid_points);
+  generate_new_connectivity(total_block_logical_coords, new_connectivity);
+  std::cout << new_connectivity << '\n';
 }
 
 // def extend_connectivity(input_file):
